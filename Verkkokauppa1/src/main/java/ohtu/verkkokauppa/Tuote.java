39174d5c39f87bb985d2638075a1c6df1400b9e6
nigosH <32242577+nigoshh@users.noreply.github.com>
2018-10-26 14:07:09 +0300

    poistettu ylimääräiset importit

--- Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Tuote.java
+++ Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Tuote.java
@@ -1,4 +1,3 @@
-
 package ohtu.verkkokauppa;
 
 public class Tuote {
@@ -15,7 +14,7 @@ public class Tuote {
     @Override
     public boolean equals(Object o) {
         Tuote t = (Tuote)o;
-        
+
         return id==t.id;
     }
 
@@ -30,6 +29,6 @@ public class Tuote {
     @Override
     public String toString() {
         return nimi;
-    }            
-    
+    }
+
 }
